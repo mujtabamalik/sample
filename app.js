ab47103df6d94b16485b4bdfd47497bb6e1a4cee
mujtabamalik <103176693+mujtabamalik@users.noreply.github.com>
2022-04-14 00:59:08 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const http = require('http')
+const port = 8080;
+const Rx = require('rxjs')
+const fs =require ('fs')
+const { fromEvent }= require( 'rxjs');
+const { map } =require('rxjs/operators');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+      "Content-type": "text/html"
+    });
+  
+    const html = fs.readFile(__dirname + "/index.html", function (error, html) {
+      if (error) {
+        throw error;
+      }
+      const requests = Rx.Observable.fromEvent(server, 'request', Array.of);
+
+      requests.subscribe(([request, response]) => {
+    
+        console.log('new request', request.url);
+
+    });
+    
+      res.end(html);
+    });
+  });
+
+  
+
+
+
+const requests = fromEvent(server, 'request', Array.of);
+
+requests.subscribe(([request, response]) => {
+    
+    console.log('new request', request.url);
+
+});
+
+server.listen(port, (err) => {
+    if (err) return console.log('Error:', err);
+
+    console.log(`Listening on ${port}`);
+})
+
